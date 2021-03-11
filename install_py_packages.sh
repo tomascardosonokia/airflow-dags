e7744e4aa4190b1364fc7315d8a0bf97daee3f39
tomascardosonokia <77099045+tomascardosonokia@users.noreply.github.com>
2021-03-11 11:54:44 +0000

    Update install_py_packages.sh

--- install_py_packages.sh
+++ install_py_packages.sh
@@ -1,3 +1,5 @@
-sudo /usr/bin/anaconda/bin/pip install azure-storage-blob
+sudo /usr/bin/anaconda/bin/pip install cryptography==3.3.2
 
-sudo /usr/bin/anaconda/bin/pip install azure-storage-file-datalake
+sudo /usr/bin/anaconda/bin/pip install azure-storage-blob==12.6.0
+
+sudo /usr/bin/anaconda/bin/pip install azure-storage-file-datalake==12.2.0
